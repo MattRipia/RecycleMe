c4a2688ad581d8f882cdfe7bc7feb6ca18ab4330
Matt Ripia <mattripia@gmail.com>
2018-09-21 20:14:19 +1200

    Added database sync with items
    Signed-off-by: Matt Ripia <mattripia@gmail.com>

--- app/src/main/java/ripia/matt/recycleme/QueryRecyclingNumberFragment.java
+++ app/src/main/java/ripia/matt/recycleme/QueryRecyclingNumberFragment.java
@@ -12,8 +12,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-
 public class QueryRecyclingNumberFragment extends Fragment implements View.OnClickListener{
 
     private Button accept, cancel;
@@ -34,16 +32,16 @@ public class QueryRecyclingNumberFragment extends Fragment implements View.OnCli
         View view = inflater.inflate(R.layout.fragment_query_recycling_number, container, false);
 
         accept = view.findViewById(R.id.accept_button);
-        cancel = view.findViewById(R.id.accept_button);
-
-        doesntExistText = view.findViewById(R.id.doesnt_exist_text);
+        cancel = view.findViewById(R.id.cancel_button);
         barcode = view.findViewById(R.id.barcode_text);
-        barcode.setText("Barcode: " + globals.getCurrentItem().getBarcode());
-
+        doesntExistText = view.findViewById(R.id.doesnt_exist_text);
         input = view.findViewById(R.id.number_input);
+
+        barcode.setText("Barcode: " + globals.getCurrentItem().getBarcode());
 
         accept.setOnClickListener(this);
         cancel.setOnClickListener(this);
+
         return view;
     }
 
@@ -51,19 +49,29 @@ public class QueryRecyclingNumberFragment extends Fragment implements View.OnCli
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.accept_button:
-                // checks that the input has a valid number in it, then sets the current items recycling number
+
+                // a user has input a valid recycling number, save this number into the currentItem and update the record in the database
                 if(Integer.parseInt(input.getText().toString()) > 0 && Integer.parseInt(input.getText().toString()) < 9)
                 {
                     globals.getCurrentItem().setRecyclingNumber(Integer.parseInt(input.getText().toString()));
+                    getFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            new ScanFragment(), "scan_fragment").commit();
+
+                    // adds the current item into the database, with the used added recycling number.
+                    globals.getDatabase().addItem();
                     Toast.makeText(getActivity(), "Thank you for helping!", Toast.LENGTH_SHORT).show();
                 }
+                // a user has input a non-valid recycling number, show a toast text explaining this
                 else
                 {
                     Toast.makeText(getActivity(), "That number is not a valid recycing number", Toast.LENGTH_SHORT).show();
                 }
                 break;
+
             case R.id.cancel_button:
                 Toast.makeText(getActivity(), "Cancel Pressed", Toast.LENGTH_SHORT).show();
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ScanFragment(), "scan_fragment").commit();
                 break;
         }
     }
