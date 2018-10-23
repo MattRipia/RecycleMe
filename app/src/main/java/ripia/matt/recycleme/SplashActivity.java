f16d9473c7508c8f7f25d0e41c50d0a950bd6b49
JimDeDev <jaimeking00@gmail.com>
2018-10-23 20:51:27 +1300

    Reverting to last working build

--- app/src/main/java/ripia/matt/recycleme/SplashActivity.java
+++ app/src/main/java/ripia/matt/recycleme/SplashActivity.java
@@ -1,9 +1,16 @@
 package ripia.matt.recycleme;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  *  The splash activity it is the first activity when the app runs and is used to
@@ -15,33 +22,51 @@ import android.util.Log;
 public class SplashActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("Splash", "Splash Screen Hit");
 
         //TODO Make the splash screen show properly
         setContentView(R.layout.activity_splash);
 
-        Thread myThread = new Thread() {
+        boolean networkState = checkNetworkActive();
 
-            @Override
-            public void run() {
-                try {
-                    sleep(2000);
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-
-            }
-        };
-        myThread.start();
+        checkLocalUserExists(networkState);
     }
+
+    private boolean checkNetworkActive() {
+        boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+            connected = true;
+        }
+        return connected;
+    }
+
+    private void checkLocalUserExists(boolean networkState) {
+        Intent activityIntent;
+
+        // if the user is not found then go to the login screen
+        if (FirebaseAuth.getInstance().getCurrentUser() == null || !networkState)
+        {
+            activityIntent = new Intent(this, LoginActivity.class);
+            Log.d("Splash", "User not found");
+
+            if(!networkState) {
+                Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+        {
+            activityIntent = new Intent(this, MainActivity.class);
+            Log.d("Splash", "User Found");
+        }
+
+        startActivity(activityIntent);
+        finish();
+    }
+
 }
-
-
-
-
-
